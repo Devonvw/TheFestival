be33df375322650e6286e826b0681f4204a8bc59
DevSoft <devonvanw@gmail.com>
2023-03-11 17:09:42 +0100

    Starting on mollie

---
+++ app/packages/mollie-api-php/examples/customers/delete-customer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace _PhpScoper97bd02f76bc0;
+
+/*
+ * Delete a customer from the Mollie API.
+ */
+try {
+    /*
+     * Initialize the Mollie API library with your API key or OAuth access token.
+     */
+    require "../initialize.php";
+    $mollie->customers->delete("cst_fE3F6nvX");
+    echo "<p>Customer deleted!</p>";
+} catch (\Mollie\Api\Exceptions\ApiException $e) {
+    echo "API call failed: " . \htmlspecialchars($e->getMessage());
+}
