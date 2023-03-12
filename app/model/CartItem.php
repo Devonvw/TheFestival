53dbdbe1b4794aafe78873068ff94f8569fd7c22
DevSoft <devonvanw@gmail.com>
2023-03-12 12:44:13 +0100

    Working on cart and payment

---
+++ app/model/CartItem.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . '/Ticket.php';
+
+class CartItem {
+  public $id;
+  public $cart_id;
+  public Ticket $ticket;
+  public $created_at;
+
+  function __construct($id = null, $cart_id = null, $ticket = null, $created_at = null) {
+    if(!is_null($id) && !is_null($cart_id) && !is_null($ticket) && !is_null($created_at)) {
+      $this->id = $id;
+      $this->cart_id = $cart_id;
+      $this->ticket = $ticket;
+      $this->created_at = $created_at;
+    }
+  }
+}
+?>
