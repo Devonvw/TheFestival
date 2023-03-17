e5393fcb54cc16ea9640c6b1693ca1d1c8fab899
DevSoft <devonvanw@gmail.com>
2023-03-17 20:50:34 +0100

    Working on checkout

--- app/DAL/PaymentDAO.php
+++ app/DAL/PaymentDAO.php
@@ -9,8 +9,22 @@ class PaymentDAO
     {
         $this->DB = new DB();
     }
+
+    function createPayment($orderId, $id) {
+        $sql = "INSERT INTO order_payment (order_id, mollie_id, status) VALUES (:order_id, :mollie_id, '');";
+        $stmt = $this->DB::$connection->prepare($sql);
+        $stmt->bindValue(':mollie_id', $id, PDO::PARAM_INT);
+        $stmt->bindValue(':order_id', $orderId, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+
     function updatePaymentStatus($orderId, $status)
     {
-       
+        $sql = "UPDATE order_payment SET status = :status WHERE order_id = :order_id";
+        $stmt = $this->DB::$connection->prepare($sql);
+        $stmt->bindValue(':status', $status, PDO::PARAM_STR);
+        $stmt->bindValue(':order_id', $orderId, PDO::PARAM_INT);
+        $stmt->execute();
     }
+
 }
